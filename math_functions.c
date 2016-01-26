98de0bf997b4628bfb7ea3935326e35e2b009340
Jonathan-Stewart <p14167237@myemail.dmu.ac.uk>
2016-01-26 15:37:49 +0000

    Added math_functions.c

---
+++ math_functions.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int add(x,y) {
+	return x+y;
+}
+
+int subtract(x,y) {
+	return x-y;
+}
+
+int multiply(x,y) {
+	return x*y;
+}
+
+int divide(x,y) {
+	return x/y;
+}
+
+int main() {
+	int i = add(1,2);
+	int j = subtract(5,2);
+	int k = multiply(7,2);
+	int l = divide(9,3);
+	printf("%i\n", i);
+	printf("%i\n", j);
+	printf("%i\n", k);
+	printf("%i\n", l);
+}
